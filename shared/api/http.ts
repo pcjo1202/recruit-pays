f727cbf38c884a5b250a7317d443021fb38f19a4
박창조 <ckdwh1202@gmail.com>
2025-11-20 00:37:17 +0900

    feat(shared): add common fether func and add env

---
+++ shared/api/http.ts
@@ -0,0 +1,27 @@
+import { env } from "../config/env";
+
+const BASE_URL = env.NEXT_PUBLIC_API_BASE_URL;
+
+export const http = {
+  get: async <T>(
+    url: string,
+    options?: RequestInit
+  ): Promise<T | undefined> => {
+    const fullUrl = `${BASE_URL}/api/v1${url}`;
+
+    const response = await fetch(fullUrl, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      ...options
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.data as T;
+  }
+};
