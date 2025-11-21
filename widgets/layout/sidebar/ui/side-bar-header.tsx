d7e2e538c2043f9de95d889c998f85dce6e4e117
박창조 <ckdwh1202@gmail.com>
2025-11-21 17:36:01 +0900

    feat(sidebar): update sidebar header

--- widgets/layout/sidebar/ui/side-bar-header.tsx
+++ widgets/layout/sidebar/ui/side-bar-header.tsx
@@ -1,7 +1,7 @@
 export default function SideBarHeader() {
   return (
     <div className="border-b py-4">
-      <h1 className="text-xl font-bold">(주)올페이즈 대시보드</h1>
+      <h1 className="text-xl font-bold">대시보드</h1>
       <h3 className="text-sm text-gray-500">결제 관리 시스템</h3>
     </div>
   );
